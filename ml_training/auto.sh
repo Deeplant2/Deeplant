80da3c902b2e95e33d5fed4fa5cc9b54dac7b4bc
tool7831 <tool7831@gmail.com>
2023-11-22 13:17:24 +0000

    update

--- ml_training/auto.sh
+++ ml_training/auto.sh
@@ -1,1 +1,10 @@
-python manage.py --run 'ech_g_gender/MSE' --ex 'age-prediction' --model_cfgs 'configs/age_all_vit224_ech_g_gender.json' --epochs 50 --data_path '/home/work/AGE'
+# +
+python manage.py --run 'dp_all_vit448/MSE' --ex 'Deeplant' --model_cfgs 'configs/deeplant/dp_all_vit448.json' --epochs 50
+python manage.py --run 'dp_all_vit448_ech_c_grade/MSE' --ex 'Deeplant' --model_cfgs 'configs/deeplant/dp_all_vit448_ech_c_grade.json' --epochs 50 
+python manage.py --run 'dp_all_vit448_ech_g_gradeColor/MSE' --ex 'Deeplant' --model_cfgs 'configs/deeplant/dp_all_vit448_ech_g_gradeColor.json' --epochs 50
+python manage.py --run 'dp_all_vit448_ech_g_gradeMarbling/MSE' --ex 'Deeplant' --model_cfgs 'configs/deeplant/dp_all_vit448_ech_g_gradeMarbling.json' --epochs 50 
+python manage.py --run 'dp_all_vit448_ech_g_gradeTexture/MSE' --ex 'Deeplant' --model_cfgs 'configs/deeplant/dp_all_vit448_ech_g_gradeTexture.json' --epochs 50 
+python manage.py --run 'dp_all_vit448_ech_g_gradeSurface/MSE' --ex 'Deeplant' --model_cfgs 'configs/deeplant/dp_all_vit448_ech_g_gradeSurface.json' --epochs 50 
+python manage.py --run 'dp_all_vit448_ech_g_gradeTotal/MSE' --ex 'Deeplant' --model_cfgs 'configs/deeplant/dp_all_vit448_ech_g_gradeTotal.json' --epochs 50 
+
+
