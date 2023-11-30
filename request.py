8c669c810e82f39576cdeefca2d9aa358462155a
jeesup park <jeesup0103@gmail.com>
2023-11-30 20:18:20 +0900

    Update request.py

--- request.py
+++ request.py
@@ -5,13 +5,19 @@ from datetime import datetime
 
 
 def send_request(image_path):
-    url = "http://127.0.0.1:5000/predict"
+    url = "http://52.79.161.151:5000/predict"
     data = {"image_path": image_path}
     print(f"Data sent \t\t{datetime.now().time()}")
     response = requests.post(url, json=data)
     print(f"Data received \t\t{datetime.now().time()}")
-
-    print(response.json())
+    try:
+        result = response.json()
+        # print(result)
+        output = result.get("xai_gradeNum_imagePath")
+    except ValueError as e:
+        print("Error: Invalid JSON response from the server.")
+        print("Exception:", e)
+        return None
 
     if response.status_code == 200:
         grade = response.json()["gradeNum"]
@@ -19,19 +25,18 @@ def send_request(image_path):
         color = response.json()["color"]
         texture = response.json()["texture"]
         surfaceMoisture = response.json()["surfaceMoisture"]
-        marbling = response.json()["marbling"]
         overall = response.json()["overall"]
-        return grade, marbling, color, texture, surfaceMoisture, marbling, overall
+        return grade, marbling, color, texture, surfaceMoisture, overall
     else:
         print("Error: Failed to get a valid response from the server.")
 
 
 # Usage example:
-image_path = "https://deep-plant-image.s3.ap-northeast-2.amazonaws.com/sensory_evals/f3ddad6010f7-1.png"
-# image_path = "https://deep-plant-image.s3.ap-northeast-2.amazonaws.com/sensory_evals/QC_cow_segmentation_1%2B_069481.jpg"
+image_path = "https://xai-deeplant-image.s3.ap-northeast-2.amazonaws.com/meat_image.jpg"
 
-## grade, marbling, color, texture, surfaceMoisture, marbling, overall
-grade, marbling, color, texture, surfaceMoisture, marbling, overall = send_request(
+## grade, marbling, color, texture, surfaceMoisture, overall
+grade, marbling, color, texture, surfaceMoisture, overall = send_request(
     image_path
 )
 
+print(grade, marbling, color, texture, surfaceMoisture, overall)
